9cdd285597e937802935af3215efc45c17440935
Stephen Whipp <stephenwhippuk@gmail.com>
2019-09-02 23:52:55 +0100

    End of Day

---
+++ MyJukeBox/MyJukeBox/ViewModels/AlbumTracks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyJukeBox.ViewModels
+{
+    /// <summary>
+    /// View Model to list the album details and the tracks on it.
+    /// </summary>
+    public class AlbumTracks
+    {
+        public String Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public ICollection<Models.Track> Tracks { get; set; }
+    }
+}
