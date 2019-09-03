ca29cd2b0e155f93c03fbf687e6f3574f4a57474
Stephen Whipp <stephenwhippuk@gmail.com>
2019-09-04 00:51:07 +0100

    end of day(04/09/2019)

---
+++ MyJukeBox/MyJukeBox/ViewModels/SelectedArtist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyJukeBox.ViewModels
+{
+    public class SelectedArtist
+    {
+        public IEnumerable<Models.Artist> Artists { get; set; }
+       public string SelectedName{ get; set; }
+        public int SelectedId { get; set; }
+
+    }
+}
